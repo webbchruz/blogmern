f5eee529780649795c19b633d9f28d528ed6dddc
webbchruz <35582236+webbchruz@users.noreply.github.com>
2024-04-30 01:38:42 +0100

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-
-
+import {BrowserRouter,Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Projetcts from "./pages/Projetcts"
+import About from "./pages/About"
 function App() {
   
 
   return (
-    <div className="text-4xl font-bold underline">
-    Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/projects" element={<Projetcts/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Projetcts.jsx
@@ -0,0 +1,8 @@
+
+export default function Projetcts() {
+  return (
+    <div>
+      projects
+    </div>
+  )
+}
